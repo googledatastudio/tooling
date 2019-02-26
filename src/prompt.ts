e916cbac49a09f7ecb348ca0bfeff99dd9195990
Matt Hamrick <matthewjhamrick@gmail.com>
2019-02-26 10:44:19 -0800

    Bit of a hack, but it works for now.

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,14 +1,23 @@
 import * as analytics from './analytics';
 import * as inquirer from 'inquirer';
+import {setTimeout} from 'timers';
+import * as util from './util';
 
 export const prompt = <T>(questions: inquirer.Questions<T>): Promise<T> => {
   const promptPromise: Promise<T> = inquirer.prompt(questions);
   // This is a hackey workaround until https://github.com/SBoudrias/Inquirer.js/issues/293 is fixed.
-  (promptPromise as any).ui.rl.on('SIGINT', () => {
+  const ui = (promptPromise as any).ui;
+  const rl = ui.rl;
+  // remove the original onForceClose listener.
+  rl.removeListener('SIGINT', ui.onForceClose);
+  rl.on('SIGINT', () => {
     analytics.trackEvent(
       analytics.Category.EXECUTION,
       analytics.Action.SIGINT_STOP
     );
+    util.pause(200).then(() => {
+      ui.onForceClose();
+    });
   });
   return promptPromise;
 };
