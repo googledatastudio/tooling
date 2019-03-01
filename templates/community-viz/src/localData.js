81a7e6eb95a7faa2cbc0413b4977854aa65a3e8e
Yulan Lin <yulan.lin.2014@gmail.com>
2019-03-01 11:39:49 -0800

    edited README and scripts

--- templates/community-viz/src/localData.js
+++ templates/community-viz/src/localData.js
@@ -5,4 +5,68 @@
 /* SETUP: Copy and paste the message printed
  * to the div in the initial visualization
  */
-export const message = {};
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimID": [
+          "25-54"
+        ],
+        "metricID": [
+          128863172
+        ]
+      },
+      {
+        "dimID": [
+          "0-14"
+        ],
+        "metricID": [
+          61175933
+        ]
+      },
+      {
+        "dimID": [
+          "65+"
+        ],
+        "metricID": [
+          51055052
+        ]
+      },
+      {
+        "dimID": [
+          "15-24"
+        ],
+        "metricID": [
+          43351778
+        ]
+      },
+      {
+        "dimID": [
+          "55-64"
+        ],
+        "metricID": [
+          42179856
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "dimID": [
+      {
+        "id": "qt_nzqx6a0xvb",
+        "name": "Age Group",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metricID": [
+      {
+        "id": "qt_8isx6a0xvb",
+        "name": "Population",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {}
+};
