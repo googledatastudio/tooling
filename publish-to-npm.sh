8335bd8397766efcf491f8c3a09e1594194215c9
Matt Hamrick <matthewjhamrick@gmail.com>
2019-03-14 15:35:36 -0700

    Update publish script to also push after it is done.

--- publish-to-npm.sh
+++ publish-to-npm.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Install needed dependencies
 yarn install
+# Build code
 yarn build
+# Create a new version
 yarn version
+# Create a new publish token
 npm login --registry https://wombat-dressing-room.appspot.com
+# Publish new version to npm
 npm publish --registry https://wombat-dressing-room.appspot.com
+# Push the new version
+git push
