ddc051118a72a9c390633422522a433fa9fa33a8
Matt Hamrick <mjhamrick@google.com>
2019-03-21 15:08:04 -0700

    updated publish script to check that git is up-to-date. (#9)

--- publish-to-npm.sh
+++ publish-to-npm.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+set -e
+
+if git branch | grep -qv "^\* master$"; then
+  echo "You must be on the master branch to run this script"
+  exit 1
+fi
+
+if test -n "$(git status --porcelain)"; then
+  echo "You have local changes that haven't been accounted for."
+  exit 1
+fi
+
+# Make sure local git is up-to-date.
+set -x
+git fetch --all
+set +x
+if test "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)"; then
+  echo "Your local branch is not in sync with origin/master."
+  exit 1
+fi
+
+set -x
 # Install needed dependencies
 yarn install
 # Build code
