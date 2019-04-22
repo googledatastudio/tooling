69dc63863c69c7ab7578bbbbe336153a22eea0bc
Matt Hamrick <matthewjhamrick@gmail.com>
2019-04-22 13:01:08 -0700

    Added first pass at USER_TOKEN auth.

--- templates/connector/src/USER_TOKEN_auth.js
+++ templates/connector/src/USER_TOKEN_auth.js
@@ -1,10 +1,53 @@
 var cc = DataStudioApp.createCommunityConnector();
+var USERNAME_PROPERTY_PATH = 'dscc.username';
+var TOKEN_PROPERTY_PATH = 'dscc.token';
 
-// https://devsite.googleplex.com/datastudio/connector/reference#getauthtype
+// TODO - implement your credentials validation logic here.
+function validateCredentials(username, password) {
+  cc.newDebugError()
+      .setText('Implement the validateCredentials() function in ./src/auth.js')
+      .throwException();
+}
+
+// https://developers.google.com/datastudio/connector/auth#getauthtype
 function getAuthType() {
-  var AuthTypes = cc.AuthType;
-  return cc
-    .newAuthTypeResponse()
-    .setAuthType(AuthTypes.NONE)
-    .build();
+  return cc.newAuthTypeResponse()
+      .setAuthType(cc.AuthType.USER_PASS)
+      .setHelpUrl('https://www.example.org/connector-auth-help')
+      .build();
+}
+
+// https://developers.google.com/datastudio/connector/auth#isauthvalid
+function isAuthValid() {
+  var userProperties = PropertiesService.getUserProperties();
+  var userName = userProperties.getProperty(USERNAME_PROPERTY_PATH);
+  var token = userProperties.getProperty(TOKEN_PROPERTY_PATH);
+  return validateCredentials(userName, token);
+}
+
+// https://developers.google.com/datastudio/connector/auth#setcredentials
+function setCredentials(request) {
+  var creds = request.userToken;
+  var username = creds.username;
+  var token = creds.token;
+
+  var validCreds = validateCredentials(username, token);
+  if (!validCreds) {
+    return {
+      errorCode: 'INVALID_CREDENTIALS'
+    };
+  }
+  var userProperties = PropertiesService.getUserProperties();
+  userProperties.setProperty(USERNAME_PROPERTY_PATH, username);
+  userProperties.setProperty(TOKEN_PROPERTY_PATH, token);
+  return {
+    errorCode: 'NONE'
+  };
+}
+
+// https://developers.google.com/datastudio/connector/auth#resetauth
+function resetAuth() {
+  var userProperties = PropertiesService.getUserProperties();
+  userProperties.deleteProperty(USERNAME_PROPERTY_PATH);
+  userProperties.deleteProperty(TOKEN_PROPERTY_PATH);
 }
