fe4334d232ba8ad809e987aa719f4b67bffebdb0
Matt Hamrick <matthewjhamrick@gmail.com>
2019-04-22 12:46:34 -0700

    Added first pass at OAUTH2 default implementation.

--- templates/connector/src/OAUTH2_auth.js
+++ templates/connector/src/OAUTH2_auth.js
@@ -1,10 +1,70 @@
 var cc = DataStudioApp.createCommunityConnector();
+var CLIENT_ID_PROPERTY_NAME = 'OAUTH_CLIENT_ID';
+var CLIENT_SECRET_PROPERTY_NAME = 'OAUTH_CLIENT_SECRET';
 
-// https://devsite.googleplex.com/datastudio/connector/reference#getauthtype
+// TODO - Edit these values to reflect your service.
+var SERVICE_NAME = 'your-service-name';
+var AUTHORIZATION_BASE_URL = 'your-authorization-base-url';
+var TOKEN_URL = 'your-token-url';
+
+// TODO - remove this function after setting the CLIENT_ID_PROPERTY_NAME &
+// CLIENT_SECRET_PROPERTY values in your script properties.
+(function () {
+  if (!PropertiesService.getProperty(CLIENT_ID_PROPERTY_NAME)) {
+    cc.newDebugError()
+        .setText('You must set the "' + CLIENT_ID_PROPERTY_NAME + '" script property for this project.')
+        .throwException();
+  }
+  if (!PropertiesService.getProperty(CLIENT_ID_PROPERTY_NAME)) {
+    cc.newDebugError()
+        .setText('You must set the "' + CLIENT_ID_PROPERTY_NAME + '" script property for this project.')
+        .throwException();
+  }
+})();
+
+// https://developers.google.com/datastudio/connector/auth#getauthtype
 function getAuthType() {
   var AuthTypes = cc.AuthType;
   return cc
-    .newAuthTypeResponse()
-    .setAuthType(AuthTypes.NONE)
-    .build();
+      .newAuthTypeResponse()
+      .setAuthType(AuthTypes.OAUTH2)
+      .build();
+}
+
+// https://developers.google.com/datastudio/connector/auth#get3pauthorizationurls
+function get3PAuthorizationUrls() {
+  return getOAuthService().getAuthorizationUrl();
+}
+
+// https://developers.google.com/datastudio/connector/auth#authcallback
+function authCallback(request) {
+  var authorized = getOAuthService().handleCallback(request);
+  if (authorized) {
+    return HtmlService.createHtmlOutput('Success! You can close this tab.');
+  } else {
+    return HtmlService.createHtmlOutput('Denied. You can close this tab');
+  }
+}
+
+// https://developers.google.com/datastudio/connector/auth#isauthvalid
+function isAuthValid() {
+  return getOAuthService().hasAccess();
+}
+
+// https://developers.google.com/datastudio/connector/auth#resetauth
+function resetAuth() {
+  getOAuthService().reset();
+}
+
+// Helper function to get the configured OauthService.
+function getOAuthService() {
+  var scriptProps = PropertiesService.getScriptProperties();
+
+  return OAuth2.createService(SERVICE_NAME)
+      .setAuthorizationBaseUrl(AUTHORIZATION_BASE_URL)
+      .setTokenUrl(TOKEN_URL)
+      .setClientId(scriptProps.getProperty(CLIENT_ID_PROPERTY_NAME))
+      .setClientSecret(scriptProps.getProperty(CLIENT_SECRET_PROPERTY_NAME))
+      .setPropertyStore(PropertiesService.getUserProperties())
+      .setCallbackFunction('authCallback');
 }
