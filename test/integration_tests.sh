7f4643ddfb361af467ba766c35578b9d1ad291ec
Matt Hamrick <matthewjhamrick@gmail.com>
2019-04-23 15:04:49 -0700

    One more attempt at better testing.
    Note: I'm not sure travis has the expect utility installed.

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -4,6 +4,12 @@ set -ex
 
 yarn run build;
 
+# Interative Tests
+for f in ./integration-tests/*.exp; do
+  expect "$f"
+done
+
+# Flag tests. Tests with prefixed with a `!` are expected to fail.
 ! node ../build/index.js viz -d bucket/doubleTrailingSlash// > /dev/null 2>&1
 
 ! node ../build/index.js viz -p bucket/doubleTrailingSlash// > /dev/null 2>&1
