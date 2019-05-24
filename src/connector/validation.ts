57ec07cdc8ea93eb49ddb72b2eee18f68335b831
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-24 12:05:59 -0700

    Fix for dotf is not a function

--- src/connector/validation.ts
+++ src/connector/validation.ts
@@ -1,6 +1,6 @@
 import dotf = require('dotf');
 
 export const claspAuthenticated = async (): Promise<boolean> => {
-  const dotglobal = dotf('~', 'clasprc.json');
+  const dotglobal = dotf.default('~', 'clasprc.json');
   return dotglobal.exists();
 };
