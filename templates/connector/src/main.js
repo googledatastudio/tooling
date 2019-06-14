9e193adceb9f4c31a6fb9742bc67f8bb5ee7e8df
Matt Hamrick <matthewjhamrick@gmail.com>
2019-06-14 15:18:54 -0700

    Fix links.

--- templates/connector/src/main.js
+++ templates/connector/src/main.js
@@ -1,11 +1,11 @@
 var cc = DataStudioApp.createCommunityConnector();
 
-// https://devsite.googleplex.com/datastudio/connector/reference#isadminuser
+// https://developers.google.com/datastudio/connector/reference#isadminuser
 function isAdminUser() {
   return false;
 }
 
-// https://devsite.googleplex.com/datastudio/connector/reference#getconfig
+// https://developers.google.com/datastudio/connector/reference#getconfig
 function getConfig(request) {
   var config = cc.getConfig();
 
@@ -59,12 +59,12 @@ function getFields() {
   return fields;
 }
 
-// https://devsite.googleplex.com/datastudio/connector/reference#getschema
+// https://developers.google.com/datastudio/connector/reference#getschema
 function getSchema(request) {
   return {schema: getFields().build()};
 }
 
-// https://devsite.googleplex.com/datastudio/connector/reference#getdata
+// https://developers.google.com/datastudio/connector/reference#getdata
 function getData(request) {
   // Calling `UrlFetchApp.fetch()` makes this connector require authentication.
   UrlFetchApp.fetch('https://google.com');
