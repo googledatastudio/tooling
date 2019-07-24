5c5571a12f52a623b3425b43b63d96494b2649ed
Yulan Lin <yulan.lin.2014@gmail.com>
2019-07-24 15:33:37 -0700

    reverted localMessage

--- templates/viz/src/localMessage.js
+++ templates/viz/src/localMessage.js
@@ -32,7 +32,7 @@ export const message = {
   fields: {
     dimID: [
       {
-        id: 'qt_odsbu27hzb',
+        id: 'qt_nzqx6a0xvb',
         name: 'Age Group',
         type: 'TEXT',
         concept: 'DIMENSION',
@@ -40,7 +40,7 @@ export const message = {
     ],
     metricID: [
       {
-        id: 'qt_7vz3u27hzb',
+        id: 'qt_8isx6a0xvb',
         name: 'Population',
         type: 'NUMBER',
         concept: 'METRIC',
@@ -48,119 +48,4 @@ export const message = {
     ],
   },
   style: {},
-  theme: {
-    themeFillColor: {
-      color: '#fff',
-      opacity: 1,
-    },
-    themeFontColor: {
-      color: '#616161',
-      opacity: 1,
-    },
-    themeFontFamily: 'Roboto',
-    themeAccentFillColor: {
-      color: '#4285F4',
-      opacity: 1,
-    },
-    themeAccentFontColor: {
-      color: '#EFEFEF',
-      opacity: 1,
-    },
-    themeAccentFontFamily: 'Roboto',
-    themeSeriesColor: [
-      {
-        color: '#4285F4',
-        opacity: 1,
-      },
-      {
-        color: '#DB4437',
-        opacity: 1,
-      },
-      {
-        color: '#F4B400',
-        opacity: 1,
-      },
-      {
-        color: '#0F9D58',
-        opacity: 1,
-      },
-      {
-        color: '#AB47BC',
-        opacity: 1,
-      },
-      {
-        color: '#00ACC1',
-        opacity: 1,
-      },
-      {
-        color: '#FF7043',
-        opacity: 1,
-      },
-      {
-        color: '#9E9D24',
-        opacity: 1,
-      },
-      {
-        color: '#5C6BC0',
-        opacity: 1,
-      },
-      {
-        color: '#F06292',
-        opacity: 1,
-      },
-      {
-        color: '#00796b',
-        opacity: 1,
-      },
-      {
-        color: '#c2185b',
-        opacity: 1,
-      },
-      {
-        color: '#7e57c2',
-        opacity: 1,
-      },
-      {
-        color: '#03a9f4',
-        opacity: 1,
-      },
-      {
-        color: '#8bc34a',
-        opacity: 1,
-      },
-      {
-        color: '#fdd835',
-        opacity: 1,
-      },
-      {
-        color: '#fb8c00',
-        opacity: 1,
-      },
-      {
-        color: '#8d6e63',
-        opacity: 1,
-      },
-      {
-        color: '#9e9e9e',
-        opacity: 1,
-      },
-      {
-        color: '#607d8b',
-        opacity: 1,
-      },
-    ],
-    themeIncreaseColor: {
-      color: '#388e3c',
-      opacity: 1,
-    },
-    themeDecreaseColor: {
-      color: '#f44336',
-      opacity: 1,
-    },
-    themeGridColor: {
-      color: '#e9e9e9',
-      opacity: 1,
-    },
-  },
-  interactions: {},
 };
