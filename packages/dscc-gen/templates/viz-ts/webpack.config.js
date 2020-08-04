7ab0803c96492347e149f851aaf663691edd25c2
Benjamín Eidelman <beneidel+gh@gmail.com>
2020-08-04 13:39:24 -0300

    viz-ts: support .tsx files (#169)
    .tsx as extension in webpack config,
    allows using JSX for building in visualizations

--- packages/dscc-gen/templates/viz-ts/webpack.config.js
+++ packages/dscc-gen/templates/viz-ts/webpack.config.js
@@ -36,14 +36,14 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.ts$/,
+          test: /\.tsx?$/,
           use: 'ts-loader',
           exclude: /node_modules/,
         },
       ],
     },
     resolve: {
-      extensions: ['.ts', '.js'],
+      extensions: ['.ts', '.tsx', '.js'],
     },
   },
 ];
